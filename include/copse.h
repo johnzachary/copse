a09d9a042ce43cfd4911cf23fe0678e4bc72ebaf
Douglas Creager <douglas.creager@redjack.com>
2012-10-31 10:24:13 -0400

    Public header file
    This patch adds a public header file that includes all of the Copse
    public library functions.

---
+++ include/copse.h
@@ -0,0 +1,18 @@
+/* -*- coding: utf-8 -*-
+ * ----------------------------------------------------------------------
+ * Copyright © 2012, RedJack, LLC.
+ * All rights reserved.
+ *
+ * Please see the LICENSE.txt file in this distribution for license
+ * details.
+ * ----------------------------------------------------------------------
+ */
+
+#ifndef COPSE_H
+#define COPSE_H
+
+/* include all of the parts */
+#include <copse/cps.h>
+#include <copse/round-robin.h>
+
+#endif /* COPSE_H */
